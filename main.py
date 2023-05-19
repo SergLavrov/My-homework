471d733f5d87a5348fbaa4287b6ab98b0720b52a
SergLavrov <ls500ls@mail.ru>
2023-05-19 23:53:59 +0300

    Ex. 4.72 (a)

---
+++ main.py
@@ -0,0 +1,12 @@
+# 4.72 (a)
+num1 = int(input('Enter a number 1: '))
+num2 = int(input('Enter a number 2: '))
+num3 = int(input('Enter a number 3: '))
+if num1 > num2 and num1 > num3:
+    print('number 1')
+elif num2 > num1 and num2 > num3:
+    print('number 2')
+elif num3 > num1 and num3 > num2:
+    print('number 3')
+else:
+    print('the numbers are equal')
