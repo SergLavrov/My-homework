a7283163fcd30f7f98b354ee6b22a18fdc40add6
SergLavrov <ls500ls@mail.ru>
2023-05-21 20:44:39 +0300

    Ex. 4.72 (б)

--- main.py
+++ main.py
@@ -12,14 +12,44 @@
 #     print('the numbers are equal')
 
 # 4.72 (б)
+# num1 = int(input('Enter a number 1: '))
+# num2 = int(input('Enter a number 2: '))
+# num3 = int(input('Enter a number 3: '))
+# if num1 < num2 and num1 < num3:
+#     print('number 1')
+# elif num2 < num1 and num2 < num3:
+#     print('number 2')
+# elif num3 < num1 and num3 < num2:
+#     print('number 3')
+# else:
+#     print('the numbers are equal')
+
+
+# 4.76
+num1 = int(input('Enter a number 1: '))
+num2 = int(input('Enter a number 2: '))
+result = num1 / 2
+if num1 > num2:
+    print('result = ' + str(result))
+elif num1 < num2 or num1 == num2:
+    print('error')
+
+
+# 4.78
 num1 = int(input('Enter a number 1: '))
 num2 = int(input('Enter a number 2: '))
 num3 = int(input('Enter a number 3: '))
-if num1 < num2 and num1 < num3:
-    print('number 1')
-elif num2 < num1 and num2 < num3:
-    print('number 2')
-elif num3 < num1 and num3 < num2:
-    print('number 3')
-else:
-    print('the numbers are equal')
+if num1 % 2 == 0:
+    print('Number 1 = ' + str(num1))
+if num2 % 2 == 0:
+    print('Number 2 = ' + str(num2))
+if num3 % 2 == 0:
+    print('Number 3 = ' + str(num3))
+if num1 % 2 != 0 and num2 % 2 != 0 and num3 % 2 != 0:
+    print('nothing')
+
+
+
+
+
+
